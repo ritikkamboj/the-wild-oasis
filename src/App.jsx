5e85c8031f6e6a42b34870fc89d112c89fd443bf
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-03-27 23:45:45 +0530

    making our custom HTML styled element more flexible with the use of props

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,37 @@ import GlobalStyles from "./styles/globalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
-
-
-
-const StyleApp =styled.div`
-  background-color: red;
+const StyleApp = styled.div`
+  /* background-color: red; */
   padding: 20px;
-`
+`;
 function App() {
   return (
     <>
-    <GlobalStyles/>
-     <StyleApp>
-      <Heading as="h1">App</Heading>
-      <Heading as="h2">Form</Heading>
-      <Heading as="h3">check In and Out </Heading>
-      <Button onClick={() => alert("checkin clicked")}>Check In </Button>
-      <Button onClick={() => alert("checkout clicked")}>Check out</Button>
-      <Input placeholder="Enter the number here..." type="number"/>
-    </StyleApp>
+      <GlobalStyles />
+      <StyleApp>
+        <Row type="vertical">
+        <Row type="horizontal">
+          <Heading as="h1">The Wild Oasis</Heading>
+          <div>
+            <Heading as="h2">Check in and Check Out</Heading>
+            <Button onClick={() => alert("checkin clicked")}>Check In </Button>
+            <Button onClick={() => alert("checkout clicked")}>Check out</Button>
+          </div>
+        </Row>
+
+        <Row type="vertical">
+          <Heading as="h3">Form</Heading>
+          <form>
+            <Input placeholder="Enter the number here..." type="number" />
+            <Input placeholder="Enter the number here..." type="number" />
+          </form>
+        </Row>
+        </Row>
+      </StyleApp>
     </>
-   
   );
 }
 
