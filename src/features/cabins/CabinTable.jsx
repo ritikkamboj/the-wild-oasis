7874291836bfadbd57e6b317274c7077be7e19b9
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-04-18 22:26:55 +0530

    added cabinwirs in cabin page

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import styled from "styled-components";
 import { getCabins } from '../../services/apiCabins';
 import Spinner from '../../ui/Spinner';
+import CabinRow from './CabinRow';
 
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
@@ -51,6 +52,7 @@ function CabinTable() {
       <div>Discount</div>
       <div></div>
     </TableHeader>
+    {cabins.map((cabin)=> <CabinRow cabin={cabin} key={cabin.id}/>)}
   </Table>
   );
 }
