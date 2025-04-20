d3b655f9d1653b39bfa5c488046841fa43edc817
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-04-20 23:22:44 +0530

    install the react hook form and then handleSubmit

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,12 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import { getCabins } from "../services/apiCabins";
 import CabinTable from "../features/cabins/CabinTable";
 import { useQuery } from "@tanstack/react-query";
 import Spinner from "../ui/Spinner";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
+
+  const [showForm , setShowForm] = useState(false);
   const {
     isLoading,
     data: cabins,
@@ -27,6 +31,9 @@ function Cabins() {
       </Row>
       <Row>
         <CabinTable />
+        <Button onClick={()=> setShowForm((state)=> !state)}>Add New Cabin</Button>
+        {showForm && <CreateCabinForm/>}
+
       </Row>
     </>
   );
