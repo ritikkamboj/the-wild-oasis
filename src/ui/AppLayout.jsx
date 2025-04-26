45bbceaac2a853c34ad8468f2158b8320d1f8b87
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-04-26 13:48:43 +0530

    make the outlet content in container overflow scroll

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -7,6 +7,8 @@ import styled from "styled-components";
 const Main = styled.main`
   background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
+  overflow: scroll; 
+
 `;
 
 const StyledAppLayout = styled.div`
@@ -21,7 +23,7 @@ const Container = styled.div`
   margin: 0 auto;
   display: flex;
   flex-direction: column;
-  gap: 3.2rem
+  gap: 3.2rem;
 `;
 
 function AppLayout() {
