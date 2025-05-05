35fff9e6b0f14ed873b2a7d2157d0925c6f38817
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-05-05 22:57:59 +0530

    changing addCabin to compound component way

--- src/features/cabins/AddCabins.jsx
+++ src/features/cabins/AddCabins.jsx
@@ -3,20 +3,31 @@ import Button from "../../ui/Button";
 import CreateCabinForm from "./CreateCabinForm";
 import Modal from "../../ui/Modal";
 
-function AddCabins() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
+// function AddCabins() {
+//   const [isOpenModal, setIsOpenModal] = useState(false);
 
-  return (
-    <div>
-      <Button onClick={() => setIsOpenModal((state) => !state)}>
-        Add New Cabin
-      </Button>
-      {isOpenModal && <Modal onClose={() => setIsOpenModal(false)}><CreateCabinForm onCloseModal={() => setIsOpenModal(false)}/></Modal>}
-    </div>
-  );
+//   return (
+//     <div>
+//       <Button onClick={() => setIsOpenModal((state) => !state)}>
+//         Add New Cabin
+//       </Button>
+//       {isOpenModal && <Modal onClose={() => setIsOpenModal(false)}><CreateCabinForm onCloseModal={() => setIsOpenModal(false)}/></Modal>}
+//     </div>
+//   );
+// }
+
+
+function AddCabins()
+{
+  return <Modal>
+    <Modal.Open>
+      <Button>Add New Cabin </Button>
+    </Modal.Open>
+    <Modal.Window>
+      <CreateCabinForm/>
+    </Modal.Window>
+  </Modal>
 }
-
-// added comment
 
 
 export default AddCabins;
